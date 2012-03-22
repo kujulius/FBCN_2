f1a963565d5cdcfac1f0e82f61bd33240a7823fb
Julius <kujulius@hotmail.com>
2012-03-22 01:27:17 -0400

    SSL Update Fail

--- Gemfile
+++ Gemfile
@@ -40,3 +40,4 @@ gem 'jquery-rails'
 # gem 'ruby-debug19', :require => 'ruby-debug'
 
 gem 'omniauth'
+gem 'omniauth-facebook'
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :twitter, YOUR_CONSUMER_KEY, YOUR_CONSUMER_SECRET
-provider :facebook, '254002848026312', '658ec6f91f6646659543cd1044ff7cd2', :display => 'popup'
+provider :facebook, '254002848026312', '658ec6f91f6646659543cd1044ff7cd2', :client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" } }
 end
